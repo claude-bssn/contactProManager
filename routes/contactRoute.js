983027b50d74235f0f7671697ec6f2dfe8446750
Klod <claude.buisson@evoluflor.fr>
2023-04-21 15:41:14 +0200

    add comments

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -2,26 +2,31 @@ const express = require("express");
 const router = express.Router();
 const contactController = require("../controllers/contactController");
 
-// route pour récupérer la liste des users : localhost/api/users
+// route pour récupérer la liste des contact 
 router 
   .get("/", contactController.getContact);
 
+// route pour récupérer un contact par l'id
 router 
   .get("/show/:id", contactController.getContactById);
 
+// route pour afficher le formulaire de création de contact
 router.route("/new") 
   .get(contactController.newContactGet);
 
+// route de création de contact
 router.route("/new") 
   .post(contactController.newContactPost);
 
+// route afficher le formulaire de mise a jour de contact
 router.route("/edit/:id")
   .get(contactController.updateGet);
 
+// route de mise a jour de contact
 router.route("/edit/:id")
   .post(contactController.updatePost);
 
-// route de suppression 
+// route de suppression de contact
 router.route("/delete/:id") 
   .get(contactController.deleteContact);
 
